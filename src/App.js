a1f235ab2bdeea925d9d4d7bd5547bde3aaa1cfa
EdwinWilliamArevalo <edwin.arevalo@tecsup.edu.pe>
2020-06-02 23:21:08 -0500

    commit ...

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ export default class App extends Component {
 
     state = {
         data: {},
+        country: ''
     }
+
 
     async componentDidMount(){
         const fetchedData = await fetchData(); 
@@ -18,15 +20,18 @@ export default class App extends Component {
         this.setState({data: fetchedData});
     }
 
+    handleCountryChange = async (country) => {
+        console.log(country);
+    }
+
     render() {
         const {data} = this.state;
         
         return (
             <div className={styles.container}>
-                <Cards data={data}/>
                 
-                <CountryPicker/>
-
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart/>
             </div>
         )
--- src/api/index.js
+++ src/api/index.js
@@ -27,3 +27,13 @@ export const fetchDailyData  = async () => {
         console.log(e);
     }
 }
+
+export const fetchCountries = async () => {
+    try{
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+        return countries.map((country) => country.name);
+
+    }catch(e){
+        console.log(e);
+    }
+}
